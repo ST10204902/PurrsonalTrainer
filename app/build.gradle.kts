21c9db23964494094107a900b7e8ec698004c15a
Try-Angle1134 <Jaspervann1313@gmail.com>
2024-11-01 15:18:56 +0200

    did push notifications

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,8 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.picasso:picasso:2.71828")
     implementation ("com.google.android.material:material:1.10.0")
+    implementation ("com.google.firebase:firebase-messaging:23.1.1")
+
 
     // Use Firebase BOM for managing Firebase dependencies
     implementation(platform("com.google.firebase:firebase-bom:31.5.0"))
