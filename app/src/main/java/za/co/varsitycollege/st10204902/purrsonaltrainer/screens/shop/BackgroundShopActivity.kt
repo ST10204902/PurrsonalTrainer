2c95230bb281355881a49554fed836007b155012
ST10180919 <st10180919@vcconnect.edu.za>
2024-11-03 17:47:46 +0200

    I did things

--- app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/screens/shop/BackgroundShopActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/screens/shop/BackgroundShopActivity.kt
@@ -17,5 +17,29 @@ class BackgroundShopActivity : AppCompatActivity()
         enableEdgeToEdge()
         binding = ActivityBackgroundShopBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Setup of Items
+        val background1 = binding.shopItemBackground1
+        background1.shopItemName.text = "*BG Name Here"
+        background1.shopItemImage.setImageResource(R.drawable.shop_background_1)
+        background1.milkcoinsComponent.milkcoinsAmount.reInitialiseComponent(R.color.background_price_start, R.color.background_price_end)
+
+        val background2 = binding.shopItemBackground2
+        background2.shopItemName.text = "*BG Name Here"
+        background2.shopItemImage.setImageResource(R.drawable.shop_background_2)
+        background2.milkcoinsComponent.milkcoinsAmount.reInitialiseComponent(R.color.background_price_start, R.color.background_price_end)
+
+        val background3 = binding.shopItemBackground3
+        background3.shopItemName.text = "*BG Name Here"
+        background3.shopItemImage.setImageResource(R.drawable.shop_background_3)
+        background3.milkcoinsComponent.milkcoinsAmount.reInitialiseComponent(R.color.background_price_start, R.color.background_price_end)
+
+        val background4 = binding.shopItemBackground4
+        background4.shopItemName.text = "*BG Name Here"
+        background4.shopItemImage.setImageResource(R.drawable.shop_background_4)
+        background4.milkcoinsComponent.milkcoinsAmount.reInitialiseComponent(R.color.background_price_start, R.color.background_price_end)
+
+        // Setup Coins
+        binding.backgroundShopCoins.milkcoinsAmount.reInitialiseComponent(R.color.background_balance_start, R.color.background_balance_end)
     }
 }
