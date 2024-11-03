a205649c1d635cb47ecc763769d51831403f942a
Anneme <ST10144453@vcconnect.edu.za>
2024-11-03 13:30:39 +0200

    Added all the cats

--- app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/services/CatService.kt
+++ app/src/main/java/za/co/varsitycollege/st10204902/purrsonaltrainer/services/CatService.kt
@@ -1,7 +1,6 @@
 package za.co.varsitycollege.st10204902.purrsonaltrainer.services
 
 import android.util.Log
-import android.util.TypedValue
 import za.co.varsitycollege.st10204902.purrsonaltrainer.R
 import za.co.varsitycollege.st10204902.purrsonaltrainer.backend.UserManager
 
@@ -9,28 +8,58 @@ import za.co.varsitycollege.st10204902.purrsonaltrainer.backend.UserManager
 private val CatAvatarDictionary: Map<Int, List<Int>> = mapOf(
     1 to listOf(
         R.drawable.cat_stage1,
-        R.drawable.cat_stage1_black
-    ),
+        R.drawable.cat_stage1_black,
+        R.drawable.cat_stage1_greystripey,
+        R.drawable.cat_stage1_white,
+        R.drawable.cat_stage1_tabby,
+        R.drawable.cat_stage1_michael,
+        R.drawable.cat_stage1_tuxedo,
+        ),
     2 to listOf(
         R.drawable.cat_stage2,
-        R.drawable.cat_stage2_black
-    ),
+        R.drawable.cat_stage2_black,
+        R.drawable.cat_stage2_greystripey,
+        R.drawable.cat_stage2_white,
+        R.drawable.cat_stage2_tabby,
+        R.drawable.cat_stage2_michael,
+        R.drawable.cat_stage2_tuxedo,
+        ),
     3 to listOf(
         R.drawable.cat_stage3,
-        R.drawable.cat_stage3_black
-    ),
+        R.drawable.cat_stage3_black,
+        R.drawable.cat_stage3_greystrippey,
+        R.drawable.cat_stage3_white,
+        R.drawable.cat_stage3_tabby,
+        R.drawable.cat_stage3_michael,
+        R.drawable.cat_stage3_tuxedo,
+        ),
     4 to listOf(
         R.drawable.cat_stage4,
-        R.drawable.cat_stage4_black
-    ),
+        R.drawable.cat_stage4_black,
+        R.drawable.cat_stage4_greystrippey,
+        R.drawable.cat_stage4_white,
+        R.drawable.cat_stage4_tabby,
+        R.drawable.cat_stage4_michael,
+        R.drawable.cat_stage4_tuxedo,
+        ),
     5 to listOf(
         R.drawable.cat_stage5,
-        R.drawable.cat_stage5_black
-    ),
+        R.drawable.cat_stage5_black,
+        R.drawable.cat_state5_greystrippey,
+        R.drawable.cat_state5_white,
+        R.drawable.cat_statge5_tabby,
+        R.drawable.cat_state5_michael,
+        R.drawable.cat_state5_tuxedo,
+        ),
     6 to listOf(
         R.drawable.cat_stage6,
-        R.drawable.cat_stage6_black
-    )
+        R.drawable.cat_stage6_black,
+        R.drawable.cat_state6_greystrippey,
+        R.drawable.cat_state6_white,
+        R.drawable.cat_stage6_tabby,
+        R.drawable.cat_state6_michael,
+        R.drawable.cat_state6_tuxedo,
+        )
 )
 
 fun GetCatStage(): Int
